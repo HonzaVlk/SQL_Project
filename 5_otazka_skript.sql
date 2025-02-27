34faeeab63ea0e2bc3808d974bddaa01581a882d
Jan Vlkovský <kokrhel@hotmail.com>
2025-02-27 09:47:44 +0100

    Update 5_otazka_skript.sql

--- 5_otazka_skript.sql
+++ 5_otazka_skript.sql
@@ -1,4 +1,4 @@
--- Otazka 5.
+-- Otázka č 5.
 -- Má výška HDP vliv na změny ve mzdách a cenách potravin? 
 -- Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na 
 -- cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
